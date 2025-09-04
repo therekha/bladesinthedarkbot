24f92e6c888d6a993eefa14ab42c10d324568375
Rekha Bhupatiraju <therekha@gmail.com>
2025-09-04 12:28:16 +0530

    resistance rollin

---
+++ commands/commandParser.test.js
@@ -0,0 +1,78 @@
+const {parseCommand} = require('./commandParser');
+const Math = require('mathjs');
+
+
+jest.mock('mathjs', () => ({
+  ...jest.requireActual('mathjs'),
+  random: jest.fn(),
+}));
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+jest.mock('mathjs', () => ({
+  ...jest.requireActual('mathjs'),
+  random: jest.fn()
+}));
+
+test('parseCommand handles a basic dice roll', () => {
+   Math.random
+    .mockReturnValueOnce(0.5) //4
+    .mockReturnValueOnce(0.9) // 6
+    .mockReturnValue(0.1) // 1
+
+    const message = '$3';
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', '6 from 4 **6** 1 ');
+});
+
+test('parseCommand handles too high in basic dice rolls', () => {
+    const message = '$23';
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', "I'm limited to rolling 9 dice at a time. I hope you don't mind!");
+});
+
+test.each([
+    ['$hello'],
+    ['$12abc'],
+    ['$']
+])('parseCommand handles an unrecognised command', (message) => {
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', "Command not recognised.");
+});
+
+test('parseCommand handles a resistance roll', () => {
+    Math.random
+        .mockReturnValueOnce(0.5) // 4
+        .mockReturnValueOnce(0.2); // 2
+
+    const message = '$r2';
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', '4 from **4** 2 \nTake 2 stress!');
+});
+
+test('parseCommand handles a resistance roll too high in dice', () => {
+    Math.random
+        .mockReturnValueOnce(0.5) // 4
+        .mockReturnValueOnce(0.2); // 2
+
+    const message = '$r12';
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', "You can't resist with more than 4 dice.");
+});
+
+test.each([
+    ['$rabc'],
+    ['$r12abc'],
+    ['$r'],
+])('parseCommand handles a malformed resistance roll command', (message) => {
+    const result = parseCommand(message);
+
+    expect(result.data).toHaveProperty('description', "Command not recognised.");
+});
---
+++ commands/rolling/resistanceRoll.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { resistRoll } = require('../../utilities/roller')
+const { embedReply, embedRollResult } = require('../../utilities/embedBuilder')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('resist')
+		.setDescription('Roll to resist a consequence.')	
+        .addIntegerOption(option =>
+			option
+				.setName('dice')
+				.setDescription('Number of dice being rolled - from the attribute being used (Insight, Prowess, or Resolve).')
+				.setRequired(true)),
+	async execute(interaction) { //gotta keep this function 2 lines
+		try {
+			let data = resistRoll(interaction.options.getInteger('dice')); 
+			let reply = embedRollResult(data.text, data.type);
+			await interaction.reply({ embeds: [reply] });
+		} catch (error) {
+			let reply = embedReply(error);
+			await interaction.reply({ embeds: [reply] });
+		}
+	},
+};

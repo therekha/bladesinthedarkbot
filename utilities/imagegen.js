fb572e4d9f1401db7e99f66816e4572d3de7817e
Rekha Bhupatiraju <therekha@gmail.com>
2025-09-04 16:15:25 +0530

    images wip 2

--- utilities/imagegen.js
+++ utilities/imagegen.js
@@ -1,13 +1,14 @@
 const Canvas = require('@napi-rs/canvas');
 const {picsURL, picsPath } = require('./consts')
 const {AttachmentBuilder} = require("discord.js");
+const { promises } = require('node:fs')
 
 const diceWidth = 100
 const diceHeight = 106
 
-const drawDice = async (rolls, result) => {
+const drawDice = async (rolls, result, saveLoc) => {
 	imageArray = getPics(rolls, result);
-	finalimg =  await combineDice(imageArray);
+	finalimg =  await combineDice(imageArray, saveLoc);
 	return finalimg;
 }
 
@@ -17,14 +18,14 @@ const getPics = (rolls, result) => {
     rolls.forEach((value, index) => {
         // create the array of image urls that will be used
         // plain if it's not the result
-        if(value !== result) pics.push(picsURL + 'plain/'+ value.toString() + '.png');
+        if(value !== result) pics.push(picsPath + 'plain/'+ value.toString() + '.png');
         // gold if it is
-        else if (value === result) pics.push(picsURL + 'gold/'+ value.toString() + '.png');
+        else if (value === result) pics.push(picsPath + 'gold/'+ value.toString() + '.png');
     });
 	return pics;
 }
 
-const combineDice = async (dicePics) => {
+const combineDice = async (dicePics, saveLoc) => {
 	const canvasWidth = dicePics.length < 5 ? dicePics.length * diceWidth : 5 * diceWidth;
 	const canvasHeight = Math.ceil(dicePics.length / 5 ) * diceHeight;
 	const canvas = Canvas.createCanvas(canvasWidth, canvasHeight);
@@ -39,7 +40,14 @@ const combineDice = async (dicePics) => {
 		context.drawImage(image, x, y, 100, 106)
 	});
 
-	const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'dice.png' });
+	const pngData = await canvas.encode('png') // JPEG, AVIF and WebP are also supported
+
+	if(saveLoc){
+		console.log(picsPath + saveLoc);
+  		await promises.writeFile( picsPath + saveLoc, pngData)
+	}
+
+	const attachment = new AttachmentBuilder(pngData, { name: 'dice.png' });
 	return attachment;
 }
 
--- utilities/imagegen.test.js
+++ utilities/imagegen.test.js
@@ -3,7 +3,7 @@ const {drawDice} = require('./imagegen');
  test('whats this thang doo', async () => {
     const rolls = [1, 2, 3, 4, 5];
     const result = 3;
-    const actual = await drawDice(rolls, result);
-    console.log(actual);
+    const actual = await drawDice(rolls, result, 'test_images/dice.png');
+
     expect(actual).toBeDefined();
 });
